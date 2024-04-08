8ce4563052d9462a33d5c543ab0b5f478d4d01b3
zhuwenxing <wenxing.zhu@zilliz.com>
2024-04-08 19:28:59 +0800

    set rto to 0
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/chaos/testcases/test_concurrent_request_operation_for_rolling_update.py
+++ tests/python_client/chaos/testcases/test_concurrent_request_operation_for_rolling_update.py
@@ -115,7 +115,7 @@ class TestOperations(TestBase):
             for k, v in self.health_checkers.items():
                 log.info(f"{k} rto: {v.get_rto()}")
                 rto = v.get_rto()
-                pytest.assume(rto < 30,  f"{k} rto expect 30s but get {rto}s")  # rto should be less than 30s
+                pytest.assume(rto <= 0,  f"{k} rto expect 0s but get {rto}s")  # rto should be less than 0s
 
             # if Op.insert in self.health_checkers:
             #     # verify the no insert data loss
