44fafac834d7862454d84f7d576800554dc76880
zhuwenxing <wenxing.zhu@zilliz.com>
2025-04-18 17:06:05 +0800

    update env
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/conftest.py
+++ tests/python_client/conftest.py
@@ -10,7 +10,7 @@ from utils.util_log import test_log as log
 from common.common_func import param_info
 from check.param_check import ip_check, number_check
 from config.log_config import log_config
-from utils.util_pymilvus import get_milvus, gen_unique_str, gen_default_fields, gen_binary_default_fields
+from utils.util_pymilvus import gen_unique_str, gen_default_fields, gen_binary_default_fields
 from pymilvus.orm.types import CONSISTENCY_STRONG
 
 timeout = 60
@@ -357,46 +357,6 @@ def check_server_connection(request):
 #     Milvus.insert = insert_decorator(Milvus.insert)
 #     yield
 
-
-@pytest.fixture(scope="module")
-def connect(request):
-    host = request.config.getoption("--host")
-    service_name = request.config.getoption("--service")
-    port = request.config.getoption("--port")
-    http_port = request.config.getoption("--http_port")
-    handler = request.config.getoption("--handler")
-    if handler == "HTTP":
-        port = http_port
-    try:
-        milvus = get_milvus(host=host, port=port, handler=handler)
-        # reset_build_index_threshold(milvus)
-    except Exception as e:
-        logging.getLogger().error(str(e))
-        pytest.exit("Milvus server can not connected, exit pytest ...")
-
-    def fin():
-        try:
-            milvus.close()
-            pass
-        except Exception as e:
-            logging.getLogger().info(str(e))
-
-    request.addfinalizer(fin)
-    return milvus
-
-
-@pytest.fixture(scope="module")
-def dis_connect(request):
-    host = request.config.getoption("--host")
-    service_name = request.config.getoption("--service")
-    port = request.config.getoption("--port")
-    http_port = request.config.getoption("--http_port")
-    handler = request.config.getoption("--handler")
-    if handler == "HTTP":
-        port = http_port
-    milvus = get_milvus(host=host, port=port, handler=handler)
-    milvus.close()
-    return milvus
 
 
 @pytest.fixture(scope="module")
